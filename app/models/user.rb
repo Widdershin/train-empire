fb5891577f9dfa595c27845f2043cec880274b75
Nick Johnstone <nick.johnstone@powershop.co.nz>
2014-08-12 10:17:55 +1200

    Added User#join_game and fixed user-game m2m relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
   has_many :user_games
-  has_many :games, through: :user_games, source: :user
+  has_many :games, through: :user_games, source: :game
 
   def host_game
     games.create
+  end
+
+  def join_game(game)
+    games << game
+    save!
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,18 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   it { should have_many :games }
 
-  it 'hosts a new game' do
-    user = User.create
+  let (:user) { User.create }
+  let (:game) { Game.create }
 
+  it 'hosts a new game' do
     user.host_game
 
     expect(user.games).to_not be_empty
-  end`
+  end
+
+  it 'joins a game' do
+    user.join_game game
+
+    expect(user.games).to include game
+  end
 end
