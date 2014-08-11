8fb4282099cc956a1ba679f962d911b00c9fa462
Nick Johnstone <nick.johnstone@powershop.co.nz>
2014-08-11 17:04:59 +1200

    Added broken many to many between User and Game

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811045056) do
+ActiveRecord::Schema.define(version: 20140811050235) do
 
   create_table "games", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
