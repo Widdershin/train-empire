e8f70d4e80e7207699cb0953118c3c1f856e093d
Nick Johnstone <nick.johnstone@powershop.co.nz>
2014-08-14 12:02:41 +1200

    Added GameState#add_player

--- app/models/game_state.rb
+++ app/models/game_state.rb
@@ -3,4 +3,8 @@ class GameState < ActiveRecord::Base
 
   has_many :players
   has_one :current_turn_player, class: Player
+
+  def add_player(player)
+    players << player
+  end
 end
--- spec/models/game_state_spec.rb
+++ spec/models/game_state_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe GameState, :type => :model do
   it { should belong_to :game }
   it { should have_one :current_turn_player }
   it { should have_many :players }
+
+  let (:game_state) { create :game_state }
+  let (:player) { create :player }
+
+  it 'adds a player' do
+    game_state.add_player player
+
+    expect(game_state.players).to include player
+  end
 end
