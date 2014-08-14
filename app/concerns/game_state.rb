2764648286b98b0524aa1525ee15835e76faad3c
Nick Johnstone <nick.johnstone@powershop.co.nz>
2014-08-15 09:35:23 +1200

    GameState now takes a collection of players

--- app/concerns/game_state.rb
+++ app/concerns/game_state.rb
@@ -1,2 +1,6 @@
 class GameState
+  attr_reader :players
+  def initialize(players)
+    @players = players
+  end
 end
--- spec/concerns/game_state_spec.rb
+++ spec/concerns/game_state_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe GameState, :type => :model do
+  describe 'creation' do
+    it 'takes an array of players' do
+      players = [double(:player)]
+      game_state = GameState.new players
+
+      expect(game_state.players).to eq players
+    end
+
+  end
 end
