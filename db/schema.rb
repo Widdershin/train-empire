697e5781229efa4746674062acdffc82a3d72984
Nick Johnstone <nick.johnstone@powershop.co.nz>
2014-08-14 14:44:17 +1200

    Added Action model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813233338) do
+ActiveRecord::Schema.define(version: 20140814023311) do
+
+  create_table "actions", force: true do |t|
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "games", force: true do |t|
     t.datetime "created_at"
