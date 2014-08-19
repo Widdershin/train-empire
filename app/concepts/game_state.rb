0b6ab78e5064308dcc06a1277ad22232d2a2389b
Nick Johnstone <ncwjohnstone@gmail.com>
2014-08-19 16:09:53 +1200

    GameState#make now constructs a GameState from a game

--- app/concepts/game_state.rb
+++ app/concepts/game_state.rb
@@ -3,16 +3,12 @@ class GameState
 
   def self.make(game)
     train_deck = DeckCreationService.new.make :train, game.seed
+
+    new(game.players, train_deck)
   end
 
-  def initialize(players, train_deck)
-    @players = players_to_player_states players
+  def initialize(player_states, train_deck)
+    @players = player_states
     @train_deck = train_deck
-  end
-
-private
-
-  def players_to_player_states(players)
-    players.map { |player| PlayerState.new player }
   end
 end
--- spec/concepts/game_state_spec.rb
+++ spec/concepts/game_state_spec.rb
@@ -1,34 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe GameState do
-  describe 'creation' do
-    let (:player) { double(:player, name: 'baz') }
-    let (:players) { [player] }
-    let (:train_deck) { double :train_deck }
-    let (:game_state) { GameState.new players, train_deck }
-
-    it 'takes a train_deck' do
-      expect(game_state.train_deck).to eq train_deck
-    end
-
-    it 'takes players and turns them into player state' do
-      expect(PlayerState).to receive(:new).with(players.first)
-
-      game_state
-    end
-  end
-
   describe 'make' do
-    let (:game) { double :game, seed: 10 }
+    let (:players) { double :players, map: self }
+    let (:game) { double :game, seed: 10, players: players }
+    let (:fake_deck) { double :deck }
+    let (:creation_service) { double :creation_service, make: fake_deck }
+
+    before do
+      allow(DeckCreationService).to receive(:new).and_return(creation_service)
+    end
 
     it 'creates a train deck and passes it in' do
-      fake_deck = double :deck
-      fake_creation_service = double :creation_service, make: fake_deck
-
-      allow(DeckCreationService).to receive(:new).and_return(fake_creation_service)
-
-      expect(fake_creation_service).to receive(:make).with(:train, game.seed)
-      GameState.make game
+      expect(creation_service).to receive(:make).with(:train, game.seed)
     end
+
+    it "passes in the game's players" do
+      expect(GameState).to receive(:new).with(game.players, fake_deck)
+    end
+
+    after { GameState.make game }
   end
 end
