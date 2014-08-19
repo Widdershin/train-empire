42bc04dfc17a1ba677d7f6046e4a247b837b5da0
Nick Johnstone <ncwjohnstone@gmail.com>
2014-08-19 15:08:37 +1200

    Started writing GameState#make

--- app/concepts/game_state.rb
+++ app/concepts/game_state.rb
@@ -1,6 +1,10 @@
 class GameState
   attr_reader :players, :train_deck
 
+  def self.make(game)
+    train_deck = DeckCreationService.new.make :train, game.seed
+  end
+
   def initialize(players, train_deck)
     @players = players_to_player_states players
     @train_deck = train_deck
--- spec/concepts/game_state_spec.rb
+++ spec/concepts/game_state_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe GameState do
       game_state
     end
   end
+
+  describe 'make' do
+    let (:game) { double :game, seed: 10 }
+
+    it 'creates a train deck and passes it in' do
+      fake_deck = double :deck
+      fake_creation_service = double :creation_service, make: fake_deck
+
+      allow(DeckCreationService).to receive(:new).and_return(fake_creation_service)
+
+      expect(fake_creation_service).to receive(:make).with(:train, game.seed)
+      GameState.make game
+    end
+  end
 end
