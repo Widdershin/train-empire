dfbda45b483b29f3a0c1d964232174d9378d5395
Nick Johnstone <ncwjohnstone@gmail.com>
2014-08-19 17:43:36 +1200

    Added contract test for ##from_players

--- spec/services/player_state_creation_service_spec.rb
+++ spec/services/player_state_creation_service_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe PlayerStateCreationService do
   let(:player) { double :player }
+  let(:state) { double :state }
 
   describe 'from_player' do
-    let(:state) { double :state }
-
     it 'creates a PlayerState from the player' do
-
       expect(PlayerState).to receive(:from_player).with(player)
+
       PlayerStateCreationService.from_player player
     end
 
@@ -20,12 +19,18 @@ RSpec.describe PlayerStateCreationService do
   end
 
   describe 'from_players' do
-    it 'maps the players to PlayerStates' do
-      players = [player, player, player]
+    let(:players) { [player, player, player] }
 
+    it 'maps the players to PlayerStates' do
       players.each { expect(PlayerStateCreationService).to receive(:from_player).with(player) }
 
       PlayerStateCreationService.from_players players
+    end
+
+    it 'returns the mapped states' do
+      allow(PlayerStateCreationService).to receive(:from_player).and_return(state)
+
+      expect(PlayerStateCreationService.from_players players).to eq [state, state, state]
     end
   end
 end
