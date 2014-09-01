06c1615b61c7b2631894d111307e14ed0a7cb4d8
Nick Johnstone <ncwjohnstone@gmail.com>
2014-09-02 10:31:24 +1200

    RouteCardLoaderService is now a private service in RouteDeckCreationService

--- app/services/route_deck_creation_service.rb
+++ app/services/route_deck_creation_service.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class RouteDeckCreationService
   def make(seed)
     CardDeck.new(load_cards, random: Random.new(seed)).shuffle
@@ -7,5 +9,22 @@ class RouteDeckCreationService
 
   def load_cards
     RouteCardLoaderService.new.load
+  end
+
+
+  class RouteCardLoaderService
+    ROUTE_CSV_FILE = "#{Rails.root}/data/routes.csv"
+
+    def load
+      data_from_file.map { |csv_hash| RouteCard.new(**csv_hash) }
+    end
+
+    private
+
+    def data_from_file
+      CSV.read(ROUTE_CSV_FILE, headers: true).map do |csv_row|
+        csv_row.to_hash.symbolize_keys
+      end
+    end
   end
 end
