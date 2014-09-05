e0b201849fb0ab24bfcc5449972b8612abe452c5
Nick Johnstone <ncwjohnstone@gmail.com>
2014-09-05 15:41:53 +1200

    Made GameStateCreation specs crappier but more respectful

--- spec/services/game_state_creation_service_spec.rb
+++ spec/services/game_state_creation_service_spec.rb
@@ -6,46 +6,19 @@ describe GameStateCreationService do
   let (:creation_service) { GameStateCreationService.new game }
 
   describe 'make' do
-    let(:route_deck_creation_service) { double(:route_deck_creation_service) }
-    let(:deck_creation_service) { double :deck_creation_service }
-    let(:routes_creation_service) { double :route_creation_service }
-    let(:fake_train_deck) { double :train_deck }
-    let(:fake_route_deck) { double :route_deck }
-    let(:fake_player_states) { double :fake_player_states }
-    let(:fake_routes) { double :fake_routes }
+    let (:player) { double :player, name: 'test', id: 1 }
+    let (:game) { double :game, players: [player], seed: 3 }
+    let (:game_state) { GameStateCreationService.new(game).make }
 
     it 'creates a gamestate' do
-      expect(PlayerStateCreationService)
-        .to receive(:from_players)
-        .with(game.players)
-        .and_return(fake_player_states)
+      expect(game_state.available_train_cards.count).to eq GameState::AVAILABLE_TRAIN_CARDS
+      expect(game_state.train_deck.draw).to be_a TrainCard
 
+      expect(game_state.route_deck.draw).to be_a RouteCard
 
-      allow(RouteDeckCreationService)
-        .to receive(:new)
-        .and_return(route_deck_creation_service)
+      expect(game_state.players.size).to eq game.players.count
 
-      expect(route_deck_creation_service)
-        .to receive(:make)
-        .with(game.seed)
-        .and_return(fake_route_deck)
-
-
-      allow(DeckCreationService)
-        .to receive(:new)
-        .and_return(deck_creation_service)
-
-      expect(deck_creation_service)
-        .to receive(:make)
-        .with(:train, game.seed)
-        .and_return(fake_train_deck)
-
-      expect(GameState)
-        .to receive(:new)
-        .with(fake_player_states, fake_train_deck, fake_route_deck, anything)
-        .and_return(instance_double('GameState').as_null_object)
-
-      creation_service.make
+      expect(game_state.route(1)).to_not be_nil
     end
   end
 end
