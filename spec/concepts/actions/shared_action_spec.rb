d014d21383f35a9f3e3533f470e4faacc1e3217a
Nick Johnstone <ncwjohnstone@gmail.com>
2014-09-12 15:23:47 +1200

    Added test for Action#from_action

--- spec/concepts/actions/shared_action_spec.rb
+++ spec/concepts/actions/shared_action_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 shared_examples_for 'an action' do
   subject { action }
 
+  it 'can be created from an action' do
+    action_model = Action.new
+    expect(action.class.from_action(action_model)).to be_a action.class
+  end
+
   it { should respond_to :process }
   it { should respond_to :end_of_turn? }
   it { should respond_to :valid? }
