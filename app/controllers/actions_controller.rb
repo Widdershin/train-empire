217e503cb97a644c732ebc9d368415ca46ba9afd
Nick Johnstone <ncwjohnstone@gmail.com>
2014-09-17 11:07:30 +1200

    Started working on the action controller

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -4,10 +4,21 @@ class ActionsController < ApplicationController
 
     player = game.players.find_by(user_id: current_user.id)
 
-    action = player.actions.create!(
+    unless player.id == game.state.current_player.id
+      flash[:error] = 'Wait your turn.'
+      return redirect_to game
+    end
+
+    action = player.actions.new(
       action: 'draw_train_card',
       card_index: params[:card_index]
     )
+
+    if action.to_modifier.valid? game.state.current_player, game.state
+      action.save
+    else
+      flash[:error] = 'Invalid card'
+    end
 
     redirect_to game
   end
--- spec/controllers/actions_controller_spec.rb
+++ spec/controllers/actions_controller_spec.rb
@@ -17,6 +17,34 @@ RSpec.describe ActionsController, :type => :controller do
       expect(new_modifier).to be_a StateModifiers::DrawTrainCard
       expect(new_modifier.card_index).to eq card_index
       expect(response).to redirect_to game
+      expect(request.flash[:error]).to be_nil
+    end
+
+    it "should fail if it's not my turn" do
+      user = create(:user)
+      second_user = create(:user)
+
+      game.users += [user, second_user]
+      game.save!
+
+      sign_in second_user
+
+      card_index = 1
+      post :draw_train_card, id: game.id, card_index: card_index
+
+      expect(request.flash[:error]).to_not be_nil
+    end
+
+    it "should fail if the action is invalid" do
+      user = create(:user)
+      sign_in user
+      game.users << user
+      game.save
+
+      card_index = 7
+      post :draw_train_card, id: game.id, card_index: card_index
+
+      expect(request.flash[:error]).to include 'Invalid card'
     end
   end
 
